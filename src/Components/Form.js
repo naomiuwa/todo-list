0f5d28af5d6941dd01c489d9cfcf5be818f44ea6
naomiuwa <naomi.uwamungu@gmail.com>
2023-08-05 16:14:08 +0100

    defined the state to keep track of the text typed into the text field& added onChange method to listen for this event and update the latest value

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 function Form() {
+  //this state keeps a track of the text the users types in the text
+  //'text' is the variable that stores what is being typed
+  //'setText' is the function that updates the text once the button is clicked
+  const [text, setText] = useState("");
+
   return (
     <>
-      <input type="text" />
+      <input
+        type="text"
+        //the value in the text field is equal to the 'text' variable
+        value={text}
+        //onChange detects when the value has changed and updates it to the latest value
+        onChange={(event) => setText(event.target.value)}
+      />
       <input type="submit" />
     </>
   );
