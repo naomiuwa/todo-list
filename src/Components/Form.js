1feb00b51cd3ee296a351abd2ed7187d8742054e
naomiuwa <naomi.uwamungu@gmail.com>
2023-08-09 21:25:53 +0100

    created a handleSubmit function which is called when the submit button is clicked

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,22 +1,32 @@
 import { useState } from "react";
 
 function Form() {
-  //this state keeps a track of the text the users types in the text
+  //this state keeps a track of the text the users types in the input field
   //'text' is the variable that stores what is being typed
-  //'setText' is the function that updates the text once the button is clicked
+  //'setText' is the function that updates the text variable once the submit button is clicked
   const [text, setText] = useState("");
 
+  //this function is to see the value of the 'text' variable when printed
+  function handleSubmit(event) {
+    //this stops the page from refreshing everytime the button is clicked
+    event.preventDefault();
+    console.log(text);
+    setText("");
+  }
+
   return (
-    <>
+    //when the submit button is clicked, the handleSubmit function is called
+
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         //the value in the text field is equal to the 'text' variable
         value={text}
-        //onChange detects when the value has changed and updates it to the latest value
+        //onChange detects when the value has changed and updates it to the latest value e.g. setText
         onChange={(event) => setText(event.target.value)}
       />
       <input type="submit" />
-    </>
+    </form>
   );
 }
 
